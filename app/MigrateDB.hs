c2d05e4433238c10b95c86fa5e86078164a2967f
Willis Plummer <willisplummer@gmail.com>
2018-10-05 12:49:34 -0400

    one more

--- app/MigrateDB.hs
+++ app/MigrateDB.hs
@@ -2,17 +2,17 @@
 
 module MigrateDB where
 
-import System.Environment
-import Database.Persist.Postgresql (PostgresConf(..))
+import           System.Environment
+import           Database.Persist.Postgresql    ( PostgresConf(..) )
 
-import Database (migrateDB)
-import Helpers.DatabaseURL (fromDatabaseUrl)
+import           Database                       ( migrateDB )
+import           Helpers.DatabaseURL            ( fromDatabaseUrl )
 
 main :: IO ()
 main = do
-    putStr "DATABASE_URL:" >> (putStrLn  =<< getEnv "DATABASE_URL")
-    dbUrl <- getEnv "DATABASE_URL"
-    (port :: Int) <- read <$> getEnv "PORT"
+    putStr "DATABASE_URL:" >> (putStrLn =<< getEnv "DATABASE_URL")
+    dbUrl                              <- getEnv "DATABASE_URL"
+    (port :: Int                     ) <- read <$> getEnv "PORT"
     (PostgresConf connString poolSize) <- fromDatabaseUrl 5 dbUrl
-  
+
     migrateDB connString
