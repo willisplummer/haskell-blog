e0353dbe69d6bbaaeeeb92e14d84b711a53ef7c5
Willis Plummer <willisplummer@gmail.com>
2019-04-07 22:44:43 -0400

    working

--- src/API.hs
+++ src/API.hs
@@ -1,304 +1,92 @@
-{-# LANGUAGE DataKinds, DeriveGeneric, TypeFamilies, TypeOperators, OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-deprecations #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeOperators #-}
 
 module API where
 
-import           Control.Concurrent             ( forkIO )
-import           Control.Monad.IO.Class         ( liftIO )
-import           Control.Monad.Trans.Except     ( throwE )
-import           Control.Monad.Reader
-import           Crypto.BCrypt                  ( validatePassword )
-import           Data.Aeson                     ( ToJSON
-                                                , toJSON
-                                                , object
-                                                , (.=)
-                                                , FromJSON
-                                                , parseJSON
-                                                , (.:)
-                                                , withObject
-                                                , Object
-                                                )
-import           Data.Aeson.Types               ( Parser
-                                                , Pair
-                                                )
-import qualified Data.ByteString.Char8         as BS
-                                                ( ByteString )
-import qualified Data.ByteString.Char8         as B8
-                                                ( pack
-                                                , unpack
-                                                )
-import           Data.Int                       ( Int64 )
-import           Data.NewUser
-import           Data.Proxy                     ( Proxy(..) )
-import           Data.Text                      ( Text )
-import           Data.Text.Encoding             ( encodeUtf8 )
-import           Database.Persist               ( Key
-                                                , Entity
-                                                , entityVal
-                                                )
-import           Database.Persist.Sql           ( SqlBackend, toSqlKey )
-import           Database.Persist.Postgresql    ( ConnectionString(..)
-                                                , fromSqlKey
-                                                )
-import           GHC.Generics
-import           Network.Wai.Handler.Warp       ( defaultSettings
-                                                , run
-                                                , runSettings
-                                                , setBeforeMainLoop
-                                                , setPort
-                                                )
-import           Network.Wai.Middleware.Cors    ( cors
-                                                , simpleCorsResourcePolicy
-                                                , corsOrigins
-                                                , corsRequestHeaders
-                                                )
-import           Servant
-import           Servant.API                    
-import           Servant.Auth
-import           Servant.Auth.Server            ( Auth
-                                                , AuthResult(..)
-                                                , BasicAuthCfg
-                                                , CookieSettings
-                                                , JWTSettings
-                                                , SetCookie
-                                                , FromBasicAuthData
-                                                , FromJWT
-                                                , ToJWT
-                                                , acceptLogin
-                                                , fromBasicAuthData
-                                                , generateKey
-                                                , defaultJWTSettings
-                                                , defaultCookieSettings
-                                                , makeJWT
-                                                , cookieXsrfSetting
-                                                , throwAll
-                                                )
-import           System.IO
+import Control.Concurrent (forkIO)
+import Control.Monad (forever)
+import Control.Monad.Trans (liftIO)
+import Data.Aeson (FromJSON, ToJSON)
+import GHC.Generics (Generic)
+import Network.Wai.Handler.Warp (run)
+import System.Environment (getArgs)
+import Servant
+import Servant.Auth.Server
+import Servant.Auth.Server.SetCookieOrphan ()
 
-import           Schema                         ( PresentationalUser(..)
-                                                , User
-                                                , Judgeable
-                                                , Judgeable(..)
-                                                , Judgement
-                                                , Judgement(..)
-                                                , Follow
-                                                , Follow(..)
-                                                , presentationalizeUser
-                                                , userHashedPassword
-                                                )
-import           Database                       ( createGetUserPG
-                                                , createJudgeablePG
-                                                , createJudgementPG
-                                                , createFollowPG
-                                                , fetchUserByEmailPG
-                                                , fetchUserPG
-                                                , fetchUsersPG
-                                                , fetchJudgeablesPG
-                                                , fetchJudgeablePG
-                                                )
+data User = User { name :: String, email :: String }
+   deriving (Eq, Show, Read, Generic)
 
-instance ToJWT PresentationalUser
-instance FromJWT PresentationalUser
+instance ToJSON User
+instance ToJWT User
+instance FromJSON User
+instance FromJWT User
 
-data Login = Login { email :: String, password :: String }
+data Login = Login { username :: String, password :: String }
    deriving (Eq, Show, Read, Generic)
 
 instance ToJSON Login
 instance FromJSON Login
 
-type JudgeablesAPI = 
-  "judgeables" :> Get '[JSON] [Entity Judgeable]
-  :<|> "judgeables" :> Capture "id" Int64 :> Get '[JSON] (Entity Judgeable)
-  :<|> "judgeables" :> ReqBody '[JSON] Judgeable :> Post '[JSON] (Entity Judgeable)
-  :<|> "judgeables" :> Capture "id" Int64 :> "judgements" :> ReqBody '[JSON] Bool :> Post '[JSON] (Entity Judgement)
+type Protected
+   = "name" :> Get '[JSON] String
+ :<|> "email" :> Get '[JSON] String
 
-judgeablesServer :: ConnectionString -> PresentationalUser -> Server JudgeablesAPI
-judgeablesServer connString currentUser =
-  (getJudgeablesHandler connString)
-  :<|> (getJudgeableHandler connString)
-  :<|> (createJudgeableHandler connString)
-  :<|> (createJudgementHandler connString currentUser)
-  where
-    getJudgeablesHandler :: ConnectionString -> Handler [Entity Judgeable]
-    getJudgeablesHandler connString = liftIO $ fetchJudgeablesPG connString
-
-    getJudgeableHandler :: ConnectionString -> Int64 -> Handler (Entity Judgeable)
-    getJudgeableHandler connString judgeableId = do
-      mJudgeable <- liftIO $ fetchJudgeablePG connString judgeableId
-      case mJudgeable of
-        Nothing -> throwError err404
-        Just judgeable -> return judgeable
-
-    createJudgeableHandler :: ConnectionString -> Judgeable -> Handler (Entity Judgeable)
-    createJudgeableHandler connString judgeable = do
-      mNewJudgeable <- liftIO $ createJudgeablePG connString judgeable
-      case mNewJudgeable of
-        Nothing -> throwError err422
-        Just newJudgeable -> return newJudgeable
-
-    createJudgementHandler :: ConnectionString -> PresentationalUser -> Int64 -> Bool -> Handler (Entity Judgement)
-    createJudgementHandler connString currentUser judgeableId isGood = do
-      mNewJudgement <- liftIO $ createJudgementPG connString judgement
-      case mNewJudgement of
-        Nothing -> throwError err422
-        Just newJudgement -> return newJudgement
-        where
-          judgeableKey :: Key Judgeable
-          judgeableKey = toSqlKey judgeableId
-
-          judgement :: Judgement
-          judgement = Judgement judgeableKey (pId currentUser) isGood
-
-type UsersAPI =
-  "users" :> Get '[JSON] [Entity User]
-  :<|> "users" :> Capture "id" Int64 :> Get '[JSON] (Entity User)
-  :<|> "users" :> Capture "id" Int64 :> "follow" :> Servant.API.PostNoContent '[JSON] (Entity Follow)
-
-usersServer :: ConnectionString -> PresentationalUser -> Server UsersAPI
-usersServer connString currentUser =
-  getUsersHandler connString
-  :<|> getUserHandler connString
-  :<|> followUserHandler connString currentUser
-  where
-    getUsersHandler :: ConnectionString -> Handler [Entity User]
-    getUsersHandler connString = liftIO $ fetchUsersPG connString
-
-    getUserHandler :: ConnectionString -> Int64 -> Handler (Entity User)
-    getUserHandler connString id = do
-      mUser <- liftIO $ fetchUserPG connString id
-      case mUser of
-        Nothing           -> throwError err404
-        Just user -> return user
-
-    followUserHandler :: ConnectionString -> PresentationalUser -> Int64 -> Handler (Entity Follow) 
-    followUserHandler connString user followedId = do
-      mFollow <- liftIO $ createFollowPG connString follow
-      case mFollow of
-        Nothing -> throwError err422
-        Just user -> return user
-        where
-          followedKey :: Key User
-          followedKey = toSqlKey followedId
-
-          follow :: Follow
-          follow = Follow (pId currentUser) followedKey
-
-type Protected =
-  JudgeablesAPI
-  :<|> UsersAPI
 
 -- | 'Protected' will be protected by 'auths', which we still have to specify.
-protected :: ConnectionString -> AuthResult PresentationalUser -> Server Protected
+protected :: Servant.Auth.Server.AuthResult User -> Server Protected
 -- If we get an "Authenticated v", we can trust the information in v, since
 -- it was signed by a key we trust.
-protected connString (Authenticated currentUser) =
-  judgeablesServer connString currentUser
-  :<|> usersServer connString currentUser
+protected (Servant.Auth.Server.Authenticated user) = return (name user) :<|> return (email user)
 -- Otherwise, we return a 401.
-protected _ _ = throwAll err401
-
-loginHandler
-  :: CookieSettings
-  -> JWTSettings
-  -> ConnectionString
-  -> Login
-  -> Handler
-       ( Headers
-           '[Header "Set-Cookie" SetCookie, Header
-             "Set-Cookie"
-             SetCookie]
-           NoContent
-       )
-
-loginHandler cookieSettings jwtSettings connString (Login email password) = do
-  mUser         <- liftIO $ fetchUserByEmailPG connString email
-  case mUser of
-    Nothing -> throwError err401
-    Just user -> do
-      mApplyCookies <-
-        if validatePassword (userHashedPassword $ entityVal user) $ B8.pack password
-          then
-            liftIO
-            $ acceptLogin cookieSettings jwtSettings
-            $ presentationalizeUser user
-          else throwError err401
-      case mApplyCookies of
-        Nothing           -> throwError err401
-        Just applyCookies -> return $ applyCookies NoContent
-
-signupHandler
-  :: CookieSettings
-  -> JWTSettings
-  -> ConnectionString
-  -> NewUser
-  -> Handler
-       ( Headers
-           '[Header "Set-Cookie" SetCookie, Header
-             "Set-Cookie"
-             SetCookie]
-           NoContent
-       )
-signupHandler cookieSettings jwtSettings connString newUser = do
-  mUser         <- liftIO $ createGetUserPG connString newUser
-  case mUser of
-    Nothing -> throwError err401
-    Just user -> do
-      mApplyCookies <-
-        liftIO
-        $ acceptLogin cookieSettings jwtSettings
-        $ presentationalizeUser user
-      case mApplyCookies of
-        Nothing           -> throwError err401
-        Just applyCookies -> return $ applyCookies NoContent
+protected _ = throwAll err401
 
 type Unprotected =
- ("login"
+ "login"
      :> ReqBody '[JSON] Login
      :> PostNoContent '[JSON] (Headers '[ Header "Set-Cookie" SetCookie
                                         , Header "Set-Cookie" SetCookie]
-                                       NoContent))
-  :<|>
-  ("signup"
-    :> ReqBody '[JSON] NewUser
-    :> PostNoContent '[JSON] (Headers '[ Header "Set-Cookie" SetCookie
-                                        , Header "Set-Cookie" SetCookie]
-                                        NoContent))
-unprotected
-  :: CookieSettings -> JWTSettings -> ConnectionString -> Server Unprotected
-unprotected cs jwts connString =
-  loginHandler cs jwts connString
-    :<|> signupHandler cs jwts connString
+                                       NoContent)
+  :<|> Raw
 
-type API auths = (Servant.Auth.Server.Auth auths PresentationalUser :> Protected) :<|> Unprotected
+unprotected :: CookieSettings -> JWTSettings -> Server Unprotected
+unprotected cs jwts = checkCreds cs jwts :<|> serveDirectory "example/static"
 
-server
-  :: CookieSettings -> JWTSettings -> ConnectionString -> Server (API auths)
-server cs jwts connString =
-  protected connString :<|> unprotected cs jwts connString
+type API auths = (Servant.Auth.Server.Auth auths User :> Protected) :<|> Unprotected
 
-mkApp :: ConnectionString -> IO Application
-mkApp connString = do
-  -- We generate the key for signing tokens. This would generally be persisted,
-  -- and kept safely
+server :: CookieSettings -> JWTSettings -> Server (API auths)
+server cs jwts = protected :<|> unprotected cs jwts
+
+mainWithCookies :: IO ()
+mainWithCookies = do
+  -- We *also* need a key to sign the cookies
   myKey <- generateKey
-  -- Adding some configurations. All authentications require CookieSettings to
-  -- be in the context.
+  -- Adding some configurations. 'Cookie' requires, in addition to
+  -- CookieSettings, JWTSettings (for signing), so everything is just as before
   let jwtCfg = defaultJWTSettings myKey
-      cfg    = (defaultCookieSettings { cookieXsrfSetting = Nothing }) :. jwtCfg :. EmptyContext
-      --- Here we actually make concrete
+      cfg = defaultCookieSettings{cookieXsrfSetting = Nothing} :. jwtCfg :. EmptyContext
+      --- Here is the actual change
       api = Proxy :: Proxy (API '[Cookie])
-  return $ cors corsPolicy $ serveWithContext api
-                          cfg
-                          (server (defaultCookieSettings { cookieXsrfSetting = Nothing }) jwtCfg connString)
-    where corsPolicy r = Just simpleCorsResourcePolicy { corsOrigins = Just (["http://127.0.0.1:1234", "http://localhost:1234"], True), corsRequestHeaders =  (corsRequestHeaders simpleCorsResourcePolicy) ++ ["content-type"] }
+  run 7249 $ serveWithContext api cfg (server defaultCookieSettings{cookieXsrfSetting = Nothing} jwtCfg)
 
-
-runServer :: Int -> ConnectionString -> IO ()
-runServer port connString = do
-  putStrLn "connString:"
-  putStrLn $ B8.unpack connString
-  let settings =
-        setPort port
-          $ setBeforeMainLoop
-            (hPutStrLn stderr ("listening on port " ++ show port))
-            defaultSettings
-  runSettings settings =<< mkApp connString
+-- Here is the login handler
+checkCreds :: CookieSettings
+           -> JWTSettings
+           -> Login
+           -> Handler (Headers '[ Header "Set-Cookie" SetCookie
+                                , Header "Set-Cookie" SetCookie]
+                               NoContent)
+checkCreds cookieSettings jwtSettings (Login "Ali Baba" "Open Sesame") = do
+   -- Usually you would ask a database for the user info. This is just a
+   -- regular servant handler, so you can follow your normal database access
+   -- patterns (including using 'enter').
+   let usr = User "Ali Baba" "ali@email.com"
+   mApplyCookies <- liftIO $ acceptLogin cookieSettings jwtSettings usr
+   case mApplyCookies of
+     Nothing           -> throwError err401
+     Just applyCookies -> return $ applyCookies NoContent
+checkCreds _ _ _ = throwError err401
